779ac887e3ab964eaa72ea6943ab42c57799aba3
Rodrigo Wasem <igowasem@gmail.com>
2021-11-20 17:11:03 -0300

    Primeiro commit

---
+++ LetsLike/Configurations/UsuarioConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LetsLike.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsLike.Configurations
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder) 
+        {
+            // setando o ID como chave primária
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ LetsLike/Data/LetsLikeContext.cs
@@ -0,0 +1,39 @@
+﻿using LetsLike.Configurations;
+using LetsLike.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsLike.Data
+{
+    public class LetsLikeContext : DbContext
+    {
+        DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+
+        public LetsLikeContext(DbContextOptions<LetsLikeContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connection = @"Server=localhost\\SQLEXPRESS;Database=letslike;Trusted_Connection=True;";
+
+                optionsBuilder.UseSqlServer(connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration);
+            modelBuilder.ApplyConfiguration(new ProjetoConfiguration);
+
+        }
+
+
+    }
+}
